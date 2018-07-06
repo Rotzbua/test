a3052126362e6581f7b38a7164a499647c6eebb7
Rotzbua <Rotzbua@users.noreply.github.com>
2018-07-06 03:07:42 +0200

    Update .deploy_remote.sh

--- .deploy_remote.sh
+++ .deploy_remote.sh
@@ -7,7 +7,7 @@ CONFIG_REMOTE_BRANCH=${CONFIG_REMOTE_BRANCH:='auto_remote'}
 TARGET=${PWD}
 TARGET=$TRAVIS_BUILD_DIR
 # care, do not run script more than once a day!
-deployTag=`date +%Y.%m.%d`
+deployTag=$(date +%Y.%m.%d)
 
 # setup ssh-agent and provide the GitHub deploy key
 eval "$(ssh-agent -s)"
@@ -24,7 +24,7 @@ echo "[ok] ssh setup done"
 # Fetch git repo
 echo "[info] fetch repo"
 
-cd ${TARGET}
+cd "${TARGET}"
 
 # setup commit user
 git config user.name "$CONFIG_REMOTE_NAME"
@@ -48,13 +48,13 @@ find ! -path '*/.*' -delete
 
 latestTag=$(git ls-remote --tags -q ${COPY_GIT}.git | sort -t '/' -k 3 -V | awk -F'/' '{ print $3 }' | tail -n 1)
 
-echo ${latestTag}
+echo "${latestTag}"
 
 latestHash=$(git ls-remote --tags -q | sort -t '/' -k 3 -V | awk -F' ' '{ print $1 }' | tail -n 1)
 
-echo ${latestHash}
+echo "${latestHash}"
 
-git clone --single-branch --depth 1 -b ${latestTag} ${COPY_GIT}.git
+git clone --single-branch --depth 1 -b "${latestTag}" ${COPY_GIT}.git
 
 cd grbl
 
@@ -63,11 +63,11 @@ cd grbl
 #git checkout $latestTag
 
 # move license
-mv COPYING ${TARGET}/LICENSE
+mv COPYING "${TARGET}"/LICENSE
 # move doc
-mv README.md ${TARGET}/README2.md
-mkdir -p ${TARGET}/extras
-mv doc/ ${TARGET}/extras/
+mv README.md "${TARGET}"/README2.md
+mkdir -p "${TARGET}"/extras
+mv doc/ "${TARGET}"/extras/
 
 # enter source code folder
 cd grbl
@@ -79,41 +79,41 @@ cd grbl
 #cd ..
 #rm -r examples
 #rm ${TARGET}/examples/ -r -f
-mv examples/ ${TARGET}/
+mv examples/ "${TARGET}"/
 
 # move source files
 #rm ${TARGET}/src/ -r -f
-mkdir -p ${TARGET}/src
-mv *.h ${TARGET}/src
-mv *.c ${TARGET}/src
-mv *.hpp ${TARGET}/src || true
-mv *.cpp ${TARGET}/src || true
+mkdir -p "${TARGET}"/src
+mv *.h "${TARGET}"/src
+mv *.c "${TARGET}"/src
+mv *.hpp "${TARGET}"/src || true
+mv *.cpp "${TARGET}"/src || true
 
 # remove temporary downloaded git
-cd ${TARGET}
+cd "${TARGET}"
 rm grbl/ -r -f
 
-if ! [[ $(git status --porcelain | grep --invert-match -e "README\.md") ]]; then
+if ! [[ $(git status --porcelain | grep --invert-match -e "README.md") ]]; then
   # No changes
   echo "[exit] no changes from remote git"
   exit
 fi
 
 # create readme
-echo -e "# This is a autodeployed library\n"> ${TARGET}/README.md
-echo -e "Remote repository: ${COPY_GIT}\n" >> ${TARGET}/README.md
-echo -e "Remote tag: [${latestTag}](${COPY_GIT}/tree/${latestTag})\n" >> ${TARGET}/README.md
-echo -e "Local tag: [${deployTag}](./tree/${deployTag})\n" >> ${TARGET}/README.md
-echo -e "Original readme: [REAMDE2.md](./blob/README2.md)\n" >> ${TARGET}/README.md
-echo -e "License: [LICENSE](./blob/LICENSE)\n" >> ${TARGET}/README.md
-echo -e "## Restrictions\n" >> ${TARGET}/README.md
-echo -e "* No bugfixes" >> ${TARGET}/README.md
-echo -e "* No issue tracker" >> ${TARGET}/README.md
-echo -e "* No support\n" >> ${TARGET}/README.md
-echo -e "## Features\n" >> ${TARGET}/README.md
-echo -e "* Automatically deploys newest tag of the according git" >> ${TARGET}/README.md
-echo -e "* Adds support for Arduino library\n" >> ${TARGET}/README.md
-echo -e "## Autor: Rotzbua" >> ${TARGET}/README.md
+echo -e "# This is a autodeployed library\n"> "${TARGET}"/README.md
+echo -e "Remote repository: ${COPY_GIT}\n" >> "${TARGET}"/README.md
+echo -e "Remote tag: [${latestTag}](${COPY_GIT}/tree/${latestTag})\n" >> "${TARGET}"/README.md
+echo -e 'Local tag: [${deployTag}](./tree/${deployTag})\n' >> "${TARGET}"/README.md
+echo -e 'Original readme: [REAMDE2.md](./blob/README2.md)\n' >> "${TARGET}"/README.md
+echo -e 'License: [LICENSE](./blob/LICENSE)\n' >> "${TARGET}"/README.md
+echo -e '## Restrictions\n' >> "${TARGET}"/README.md
+echo -e "* No bugfixes" >> "${TARGET}"/README.md
+echo -e "* No issue tracker" >> "${TARGET}"/README.md
+echo -e "* No support\n" >> "${TARGET}"/README.md
+echo -e "## Features\n" >> "${TARGET}"/README.md
+echo -e "* Automatically deploys newest tag of the according git" >> "${TARGET}"/README.md
+echo -e "* Adds support for Arduino library\n" >> "${TARGET}"/README.md
+echo -e "## Autor: Rotzbua" >> "${TARGET}"/README.md
 
 # commit changes
 git add -A
